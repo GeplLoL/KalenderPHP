059c096f3aad030e319bd1b8686175057ac9d289
Your Name <you@example.com>
2024-10-30 14:35:40 +0200

    commited

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,10 @@
 <?php
 
-// Database connection
-$servername = "d123173.mysql.zonevs.eu";
-$username = "d123173_maksdot";
-$password = "Tark123456";
-$dbname = "d123173_calendar";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Database connection using PDO for Azure SQL
+try {
+    $conn = new PDO("sqlsrv:server=tcp:calendarphp.database.windows.net,1433;Database=calendar", "Oleksandr", "{Abkillio2007}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Error connecting to SQL Server.";
+    die(print_r($e));
 }
